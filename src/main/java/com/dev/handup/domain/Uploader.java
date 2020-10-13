adb23fe9b9615b4e7f2897b9ab2461780d36a054
rwh <fkrns1744@naver.com>
2020-10-13 14:53:58 +0900

    [Update: 세션 저장소 DB 변경]

--- src/main/java/com/dev/handup/domain/Uploader.java
+++
@@ -1,9 +0,0 @@
-package com.dev.handup.domain;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface Uploader {
-    String upload(MultipartFile multipartFile, String dirName) throws IOException;
-}
